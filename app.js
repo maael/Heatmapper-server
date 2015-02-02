1e164859dc27daef74f9fa07c6eacf8181bf30b2
maael <mae.me.uk@gmail.com>
2015-02-02 17:16:44 +0000

    Add Procfile, use port environment variable or 3000 as default

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     pub = __dirname + '/public',
-    app = express();
+    app = express(),
+    port = process.env.PORT || 3000;
 app.use(express.static(pub));
 app.set('views',__dirname + '/views');
 app.set('view engine','jade');
@@ -8,7 +9,7 @@ app.get('/', function (req, res) {
   res.render('index');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
 
   var host = server.address().address;
   var port = server.address().port;
